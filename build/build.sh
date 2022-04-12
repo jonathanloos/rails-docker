c9fe049a996fa71c19c713531aa9849c11fa2a0c
Chris Young <chris@harled.ca>
2022-04-12 12:23:36 -0400

    support for 3.1.2

--- build/build.sh
+++ build/build.sh
@@ -1,18 +1,34 @@
 # login
 docker login --username=krsyoung
 
-# build
-docker build --no-cache -f Dockerfile.slim.ruby-2.7 -t krsyoung/rails-build:2.7.2-slim-buster .
-docker build --no-cache -f Dockerfile.slim.ruby-3.0.0 -t krsyoung/rails-build:3.0.0-slim-buster .
-docker build --no-cache -f Dockerfile.slim.ruby-3.0.1 -t krsyoung/rails-build:3.0.1-slim-buster .
-docker build --no-cache -f Dockerfile.slim.ruby-3.0.2 -t krsyoung/rails-build:3.0.2-slim-buster .
-docker build --no-cache -f Dockerfile.slim.ruby-3.0.3 -t krsyoung/rails-build:3.0.3-slim-buster .
-docker build --no-cache -f Dockerfile.slim-bullseye.ruby-3.1.0 -t krsyoung/rails-build:3.1.0-slim-bullseye .
-docker build --no-cache -f Dockerfile.slim-bullseye.ruby-3.1.1 -t krsyoung/rails-build:3.1.1-slim-bullseye .
+# 3.1.3
+docker build --no-cache -f Dockerfile.slim-bullseye.ruby-3.1.3 -t krsyoung/rails-build:3.1.3-slim-bullseye .
+docker push krsyoung/rails-build:3.1.3-slim-bullseye
 
-docker push krsyoung/rails-build:3.0.0-slim-buster
-docker push krsyoung/rails-build:3.0.1-slim-buster
-docker push krsyoung/rails-build:3.0.2-slim-buster
-docker push krsyoung/rails-build:3.0.3-slim-buster
-docker push krsyoung/rails-build:3.1.0-slim-bullseye
+# 3.1.2
+docker build --no-cache -f Dockerfile.slim-bullseye.ruby-3.1.2 -t krsyoung/rails-build:3.1.2-slim-bullseye .
+docker push krsyoung/rails-build:3.1.2-slim-bullseye
+
+# 3.1.1
+docker build --no-cache -f Dockerfile.slim-bullseye.ruby-3.1.1 -t krsyoung/rails-build:3.1.1-slim-bullseye .
 docker push krsyoung/rails-build:3.1.1-slim-bullseye
+
+# 3.1.0
+docker build --no-cache -f Dockerfile.slim-bullseye.ruby-3.1.0 -t krsyoung/rails-build:3.1.0-slim-bullseye .
+docker push krsyoung/rails-build:3.1.0-slim-bullseye
+
+# 3.0.3
+docker build --no-cache -f Dockerfile.slim.ruby-3.0.3 -t krsyoung/rails-build:3.0.3-slim-buster .
+docker push krsyoung/rails-build:3.0.3-slim-buster
+
+# 3.0.2
+docker build --no-cache -f Dockerfile.slim.ruby-3.0.2 -t krsyoung/rails-build:3.0.2-slim-buster .
+docker push krsyoung/rails-build:3.0.2-slim-buster
+
+# 3.0.1
+docker build --no-cache -f Dockerfile.slim.ruby-3.0.1 -t krsyoung/rails-build:3.0.1-slim-buster .
+docker push krsyoung/rails-build:3.0.1-slim-buster
+
+# 3.0.0
+docker build --no-cache -f Dockerfile.slim.ruby-3.0.0 -t krsyoung/rails-build:3.0.0-slim-buster .
+docker push krsyoung/rails-build:3.0.0-slim-buster
