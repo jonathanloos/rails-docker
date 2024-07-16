0004e4d99e1eae0a56d69092a50d51a012211ab5
Chris Young <chris@harled.ca>
2024-07-15 21:06:37 -0400

    fix: latest

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # rails-docker
 
-This repository contains helpful Dockerfiles for building (pipeline) and running Ruby on Rails applications.
+This repository contains helpful Dockerfiles for building (CI pipeline) and running Ruby on Rails applications.
 
 There are two images published from this repository:
 
 **rails-build**
-This image is intended to be used when building your rails application via a CI pipeline. It assumes that
-you are using the "old" javascript pipeline (yarn, webpacker).
+This image is intended to be used when building a rails application via a CI pipeline. It assumes that
+the application is using the "old" javascript pipeline (yarn, webpacker).
 
 
 **rails-run**
 This image is to be used for running the actual application. It includes the libraries and packages typically 
 necessary to run a base rails application.
+
+## Releasing
+
+New images can be released with the `dx.sh` script. To see all of the available commands run:
+
+1. `source dx.sh`
+2. `help`
 
 
--- dx.sh
+++ dx.sh
@@ -2,10 +2,10 @@
 DOCKER_HUB_ACCOUNT="krsyoung"
 
 function help () {
-  echo "** rails build tooling **"
+  echo "** rails docker image tooling **"
   echo
   echo "commands:"
-  echo "  build <image> <version>   - build the docker image and tag as latest"
+  echo "  build <image> <version>   - build the image and tag as latest"
   echo "  freshen <image> <version> - build the image, tag as latest and push to Docker Hub"
   echo "  push <image> <version>    - push the version to Docker Hub"
   echo "  release <image> <version> - add the version tag to latest and push to Docker Hub"
@@ -13,14 +13,14 @@ function help () {
   echo "  images                    - list the available images"
   echo
   echo "example (test and release): "
-  echo "1. install latest packages for rails-run 3.3.1 image"
-  echo "   > freshen rails-run 3.3.1"
-  echo "2. after testing, promote latest image to new 3.3.1 tag"
-  echo "   > release rails-run 3.3.1"
+  echo "1. install latest packages for rails-run 3.3.4 image"
+  echo "   > freshen rails-run 3.3.4"
+  echo "2. after testing, promote latest image to new 3.3.4 tag"
+  echo "   > release rails-run 3.3.4"
   echo
-  echo "example (full send): "
-  echo "1. build, tag and deploy an updated image rails-run 3.3.1 image"
-  echo "   > update rails-run 3.3.1"
+  echo "example (full send, skips testing): "
+  echo "1. build, tag and deploy an updated image rails-run 3.3.4 image"
+  echo "   > update rails-run 3.3.4"
   echo
 }
 
@@ -36,26 +36,13 @@ function login () {
   docker login --username=$DOCKER_HUB_ACCOUNT
 }
 
-# tag an exiting image as latest and push
-function release () {
-  image=$1
-  version=$2
-  [ -z "$image" ] && echo "Error: need an image." && return 1
-  [ -z "$version" ] && echo "Error: need an version." && return 1
-  echo "Releasing $image:$version"
-
-  # tag the specific version with latest  
-  push $image $version
-  docker tag $DOCKER_HUB_ACCOUNT/$image:$version $DOCKER_HUB_ACCOUNT/$image:latest
-  push $image latest
-}
-
 # build image
 function build () {
   image=$1
   version=$2
   [ -z "$image" ] && echo "Error: need an image." && return 1
   [ -z "$version" ] && echo "Error: need an version." && return 1
+
   echo "Building $image:$version"
 
   dockerfile="$image/Dockerfile-$version"
@@ -66,16 +53,6 @@ function build () {
 
   # build, without cache and apply a version tag based on the ruby version
   docker build --no-cache -f $dockerfile -t $DOCKER_HUB_ACCOUNT/$image:$version $image/
-}
-
-function push () {
-  image=$1
-  version=$2
-  [ -z "$image" ] && echo "Error: need an image." && return 1
-  [ -z "$version" ] && echo "Error: need an version." && return 1
-
-  echo "Pushing $image:$version"
-  docker push "$DOCKER_HUB_ACCOUNT/$image:$version"
 }
 
 # build image and tag with current date
@@ -91,6 +68,30 @@ function freshen () {
   push $image latest
 }
 
+function push () {
+  image=$1
+  version=$2
+  [ -z "$image" ] && echo "Error: need an image." && return 1
+  [ -z "$version" ] && echo "Error: need an version." && return 1
+
+  echo "Pushing $image:$version"
+  docker push "$DOCKHER_HUB_ACCOUNT/$image:$version"
+}
+
+# tag an exiting image as latest and push
+function release () {
+  image=$1
+  version=$2
+  [ -z "$image" ] && echo "Error: need an image." && return 1
+  [ -z "$version" ] && echo "Error: need an version." && return 1
+  echo "Releasing $image:$version"
+
+  # tag the specific version with latest  
+  push $image $version
+  docker tag $DOCKHER_HUB_ACCOUNT/$image:$version $DOCKHER_HUB_ACCOUNT/${image}:latest
+  push $image latest
+}
+
 # build image and tag with current date
 function update () {
   image=$1
@@ -102,7 +103,6 @@ function update () {
 
   build $image $version
   release $image $version
-  release $image latest
 }
 
 help
